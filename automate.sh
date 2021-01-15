d17a7f712be16e6dd5a0ac5bfd8b4a2086f4c8f9
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-01-15 12:24:45 +0300

    automate.sh :: Count seconds from start to end.

--- automate.sh
+++ automate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+start=$(date +%s)
 kubectl delete deployments --all
 kubectl delete svc --all
 eval $(minikube docker-env)
@@ -7,3 +8,5 @@ kubectl apply -f manifest.yaml
 latest_pod=$(kubectl get pods --sort-by=.metadata.creationTimestamp -o jsonpath="{.items[-1].metadata.name}")
 while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 kubectl exec $latest_pod -- bash -c "yarn test --tags '@Component'"
+end=$(date +%s)
+echo $(($end-$start))
