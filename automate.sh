240e7b3d4d935c5c83095e2ae47ff645e9b80ed4
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-02-25 09:33:10 +0300

    Fail 'standingsService is running' in EndToEnd tests.

--- automate.sh
+++ automate.sh
@@ -7,6 +7,7 @@ docker build -t segment17/boxerservice .
 kubectl apply -f manifest.yaml
 latest_pod=$(kubectl get pods --sort-by=.metadata.creationTimestamp -o jsonpath="{.items[-1].metadata.name}")
 while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
-kubectl exec $latest_pod -- bash -c "yarn test --tags '@Component'"
+kubectl exec $latest_pod -- bash -c "yarn test --tags '@EndToEnd'"
+# kubectl exec $latest_pod -- bash -c "yarn test --tags '@Component'"
 end=$(date +%s)
 echo $(($end-$start))
--- features/step_definitions/EndToEnd.js
+++ features/step_definitions/EndToEnd.js
@@ -1,9 +1,36 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
-
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_PATH = __dirname + "/../../proto/boxer-service.proto";
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+const boxerservice_package = grpc.loadPackageDefinition(packageDefinition).boxerservice_package;
+const standingsservice_package = grpc.loadPackageDefinition(packageDefinition).standingsservice_package;
+const matchservice_package = grpc.loadPackageDefinition(packageDefinition).matchservice_package;
 Given('the {string} service is running', function (string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  try {
+    if (string == "boxer") {
+      var boxerserviceClient = new boxerservice_package.BoxerService("0.0.0.0" + ":" + process.env.BOXER_SERVICE_SERVICE_PORT,grpc.credentials.createInsecure());
+    } else if (string == "standings") {
+      var standingsserviceClient = new standingsservice_package.StandingsService("0.0.0.0" + ":" + process.env.STANDINGS_SERVICE_SERVICE_PORT,grpc.credentials.createInsecure());
+    } else if (string == "match") {
+      var matchserviceClient = new matchservice_package.MatchService("0.0.0.0" + ":" + process.env.MATCH_SERVICE_SERVICE_PORT,grpc.credentials.createInsecure());
+    } else {
+      assert(false);
+    }
+  } catch (error) {
+    console.log(error);
+    assert(false);
+  }
+  return true;
 });
 
 Given('the boxer with the id {string} has matches', function (string) {
