505673e51dd243d960dd864f54f72bc5e90c0742
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-04-08 10:47:59 +0300

    Automate local scripts update.

--- automate_local_1.sh
+++ automate_local_1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 521166 --only=features
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 531603 --only=features
 yarn test --exit
-hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 521166 --push-format cucumber-json
+hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 531603 --push-format cucumber-json
 end=$(date +%s)
 echo $(($end-$start))
