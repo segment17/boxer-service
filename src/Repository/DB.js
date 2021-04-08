45b61bc67e5e46856269ade8cf2cb471bef90a94
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-04-08 11:02:57 +0300

    Init DB information.

---
+++ src/Repository/DB.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var mysql = require('mysql');
+
+//TODO: Create database and table during DevOps
+/*
+CREATE TABLE boxers (
+  id INT NOT NULL AUTO_INCREMENT,
+  fullName VARCHAR(64) NOT NULL,
+  birthDate BIGINT NOT NULL,
+  height INT NOT NULL,
+  weight DOUBLE NOT NULL,
+  PRIMARY KEY (id)
+);
+*/
+
+const connectionSetup = {
+  host: process.env.BOXER_MYSQL_SERVICE_SERVICE_HOST != undefined ? process.env.BOXER_MYSQL_SERVICE_SERVICE_HOST : "localhost",
+  user: "root",
+  password: "root",
+  database: "boxerservice"
+};
+
+var connection = mysql.createConnection(connectionSetup);
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = { connection };
