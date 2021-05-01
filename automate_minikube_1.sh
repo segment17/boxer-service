b855a66c240fbf00263ababa7253d424b0748e08
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-05-01 15:22:09 +0300

    Fixes.

--- automate_minikube_1.sh
+++ automate_minikube_1.sh
@@ -10,8 +10,8 @@ kubectl apply -f manifest.yaml
 latest_pod=$(kubectl get pods --sort-by=.metadata.creationTimestamp -o jsonpath="{.items[-1].metadata.name}")
 while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for pod..." && sleep 1; done
 # kubectl exec $latest_pod -- bash -c "yarn test --tags '@EndToEnd'"
-kubectl exec $latest_pod -- bash -c "yarn test --exit" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
-kubectl cp $latest_pod:test/results.json test/results.json
+# kubectl exec $latest_pod -- bash -c "yarn test --exit" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
+# kubectl cp $latest_pod:test/results.json test/results.json
 hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 538207 --push-format cucumber-json
 end=$(date +%s)
 echo $(($end-$start))
