128a89f3414f7d26adff3adcdcb8a828b21f6e1b
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-05-17 10:18:27 +0300

    MatchServiceGatewayScenarioTester :: init.

---
+++ test/ScenarioTesters/Gateway/MatchServiceGatewayScenarioTester.js
@@ -0,0 +1,33 @@
+
+const DefaultScenarioTester = require('../DefaultScenarioTester');
+const TestFunctions = require('../../TestFunctions');
+const globalObjects = require('../../..');
+const assert = require('assert');
+
+class MatchServiceGatewayScenarioTester extends DefaultScenarioTester {
+
+  unitFunctionIsInvokedWithData(functionName, dataSource) {
+    const boxerId = TestFunctions.extractSpecifiedObjectData(dataSource);
+    if (functionName == "deleteMatchesOfBoxer") {
+      globalObjects.matchServiceGateway.deleteMatchesOfBoxer(boxerId).then(result => {
+        globalObjects.result = result;
+      });
+    }
+  }
+
+  async returnedDataIsAs(dataSource) {
+    const expectedData = TestFunctions.extractSpecifiedObjectData(dataSource);
+    await TestFunctions.waitUntilResult();
+    assert(globalObjects.result.code === expectedData.code);
+    assert(globalObjects.result.message === expectedData.message);
+  }
+
+  async thereAreMatchesSuchAs(dataSource) {
+    const matches = TestFunctions.extractSpecifiedObjectData(dataSource);
+    await globalObjects.matchServiceGateway.setupAddMatches(matches);
+    globalObjects.done = true;
+  }
+
+}
+
+module.exports = MatchServiceGatewayScenarioTester;
